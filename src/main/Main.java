f49c67f7b8bee08023f0bceb663398943555cd5e
Daniel Morales <danisan98@hotmail.es>
2021-08-02 16:35:03 -0500

    initial commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,32 +4,25 @@ import control.ControlLibreria;
 import java.util.Calendar;
 import java.util.Scanner;
 
- /*
-    Autor: Daniel Santiago Morales Alvarado
- */
+/*
+  Autor: Daniel Santiago Morales Alvarado
+*/
 public class Main {
 
-    public static void main(String[] args) {
-        ControlLibreria control = new ControlLibreria();
-        Scanner input = new Scanner(System.in);
-        String entrada;
-        do{
-            System.out.println("Ingrese un comando (escriba 'ayuda' para obtener informacion de los comandos)");
-            entrada = input.next();
-            switch(entrada){
+   public static void main(String[] args) {
+       ControlLibreria control = new ControlLibreria();
+       Scanner input = new Scanner(System.in);
+       String entrada;
+       do{
+           System.out.println("Ingrese un comando (escriba 'ayuda' para obtener informacion de los comandos)");
+           entrada = input.next();
+           switch (entrada) {
+               case "crear" -> control.agregarRegistro(control.crearAutor());
+               case "listar" -> control.buscarAutor();
+               case "detallar" -> control.detallar();
+           }
+       }while(!entrada.equalsIgnoreCase("salir"));
+       System.out.println("Adios c:");
 
-                case "crear":
-                    control.agregarRegistro(control.crearAutor());
-                    break;
-                case "listar":
-                    control.buscarAutor();
-                    break;
-                case "detallar":
-                    control.detallar();
-                    break;
-            }
-        }while(!entrada.equalsIgnoreCase("salir"));
-        System.out.println("Adios c:");
-
-    }
+   }
 }
